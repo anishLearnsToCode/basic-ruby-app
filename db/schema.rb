118da872078c5b96d4534155072bccfff608cef5
anishSachdeva <anish_@outlook.com>
2018-03-30 01:28:37 +0530

    ran rake db:mirate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329194710) do
+ActiveRecord::Schema.define(version: 20180329195510) do
 
   create_table "players", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
