f3487fc3eb4ed95dac52b2c72f4d6782939a50cf
anishSachdeva <anish_@outlook.com>
2018-03-30 12:30:32 +0530

    added validation for age in Players table

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ApplicationRecord
-  validates :first_name, :age, presence: true
+  validates :first_name, presence: true
   validates :last_name, presence: true, on: :create
+  validates :age, :numericality => {greater_than: 0, less_than_or_equal_to: 130}
 end
