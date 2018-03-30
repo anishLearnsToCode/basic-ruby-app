9f5b4f72139d7e3b8f492fa14e2558b9fd3c0e2d
anishSachdeva <anish_@outlook.com>
2018-03-30 13:35:22 +0530

    added has_one :address to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+  has_one :address
+
   validates :first_name, presence: true
   validates :last_name, presence: true, on: :create
   validates :age, :numericality => {greater_than: 0, less_than_or_equal_to: 130}
