52182683225c42d3bc966d73b41601e024cce850
anishSachdeva <anish_@outlook.com>
2018-03-30 20:12:44 +0530

    added a form in the new view in new view in person_controller. Added params code in create method inside person_controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,10 +4,14 @@ class PlayersController < ApplicationController
   end
 
   def new
-
   end
 
   def create
-
+    new_player = Player.new
+    new_player.first_name= params[:player][:first_name]
+    new_player.last_name= params[:player][:last_name]
+    new_player.age= params[:player][:age]
+    new_player.save
   end
+  
 end
