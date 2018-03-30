824c3a63a15d18b34b69a842a5accdde6ded9382
anishSachdeva <anish_@outlook.com>
2018-03-30 14:09:52 +0530

    ran rake:db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330081622) do
+ActiveRecord::Schema.define(version: 20180330083822) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street"
@@ -18,6 +18,22 @@ ActiveRecord::Schema.define(version: 20180330081622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_addresses_on_player_id"
+  end
+
+  create_table "appearances", force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_appearances_on_game_id"
+    t.index ["player_id"], name: "index_appearances_on_player_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "home"
+    t.string "away"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
