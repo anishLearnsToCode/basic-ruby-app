5ff65e8f022ca1312ae375b92b2078bfd4a32903
anishSachdeva <anish_@outlook.com>
2018-03-31 14:55:06 +0530

    created root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'players#index'
+
+  # Players Controller routes
   get 'players' => 'players#index'
 
   # To obtain individual player ID's dynamically
   get 'players/:id' => 'players#show'
+
+  # To edit individual Players
   get 'players/:id/edit' => 'players#edit'
+  patch 'players/:id' => 'players#update'
+
+  # To create a new player
   get 'players/new' => 'players#new'
   post 'players' => 'players#create'
-  patch 'players/:id' => 'players#update'
+
+  # To delete a particular player
   delete 'players/:id' => 'players#destroy'
 end
